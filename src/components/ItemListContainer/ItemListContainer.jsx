ca49ce1698d92f962d46a778376c402b97ddd7c3
Facundo <facundo.cordoba2012@gmail.com>
2022-06-04 18:03:12 -0300

    Menu con json

--- src/components/ItemListContainer/ItemListContainer.jsx
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 function ItemListContainer({ saludo }) {
   return (
-
-    <h3 style={{ color: "white", backgroundColor: "#17202A", textAlign: "center" }}>{saludo}</h3>
-
+    <div>
+      <h3 style={{ color: "white", backgroundColor: "#17202A", textAlign: "center" }}>{saludo}</h3>
+    </div>
   )
 }
 
