56cbc05132ae4ecd8e02cad3dc2f4fca883ce288
Facundo <facundo.cordoba2012@gmail.com>
2022-06-05 10:31:41 -0300

    ItemCount

--- src/components/ItemListContainer/ItemListContainer.jsx
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import ItemCount from '../ItemCount/ItemCount'
 
 function ItemListContainer({ saludo }) {
   return (
     <div>
       <h3 style={{ color: "white", backgroundColor: "#17202A", textAlign: "center" }}>{saludo}</h3>
+      <ItemCount stockTot="10" initial='1'/>
     </div>
   )
 }
